8c3743aa6b1a0d1d1f4039e9a39901e380971124
sam-amirpoor <amirpoorDev@gmail.com>
2026-07-31 20:10:53 +0330

    feat: import entities to modules

--- src/modules/monitoring/monitoring.module.ts
+++ src/modules/monitoring/monitoring.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MonitoringService } from './monitoring.service';
+import { MonitoringController } from './monitoring.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MonitoringResult } from './entities/monitoring-result.entity';
 
-@Module({})
+@Module({
+  providers: [MonitoringService],
+  controllers: [MonitoringController],
+  imports: [TypeOrmModule.forFeature([MonitoringResult])],
+})
 export class MonitoringModule {}
--- src/modules/websites/websites.module.ts
+++ src/modules/websites/websites.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { WebsitesController } from './websites.controller';
 import { WebsitesService } from './websites.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Website } from './entities/website.entity';
 
 @Module({
   controllers: [WebsitesController],
   providers: [WebsitesService],
+  imports: [TypeOrmModule.forFeature([Website])],
 })
 export class WebsitesModule {}
