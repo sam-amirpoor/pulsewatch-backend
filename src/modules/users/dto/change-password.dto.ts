e262f73e6d1cdee48c9ab9fefcd111c712dbd88b
sam-amirpoor <amirpoorDev@gmail.com>
2026-08-06 07:52:36 +0330

    fix: add IsNotEmpty decorator to change-password.dto

--- src/modules/users/dto/change-password.dto.ts
+++ src/modules/users/dto/change-password.dto.ts
@@ -1,8 +1,9 @@
-import { IsString, MaxLength, MinLength } from 'class-validator';
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class changePasswordDto {
   @IsString()
   @MinLength(8)
   @MaxLength(72)
+  @IsNotEmpty()
   readonly password!: string;
 }
