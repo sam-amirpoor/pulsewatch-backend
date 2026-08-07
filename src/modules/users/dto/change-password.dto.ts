58d59a3af19b5deeab5600c782836899e7ccf04f
sam-amirpoor <amirpoorDev@gmail.com>
2026-08-07 21:02:43 +0330

    fix: change change-password.dto

--- src/modules/users/dto/change-password.dto.ts
+++ src/modules/users/dto/change-password.dto.ts
@@ -1,6 +1,6 @@
 import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 
-export class changePasswordDto {
+export class ChangePasswordDto {
   @IsString()
   @MinLength(8)
   @MaxLength(72)
