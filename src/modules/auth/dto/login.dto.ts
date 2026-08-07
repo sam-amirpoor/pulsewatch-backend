696b18bb96b7ed88838ebda4b8d63fa03f911ce2
sam-amirpoor <amirpoorDev@gmail.com>
2026-08-07 20:17:52 +0330

    fix: add IsString decorator to password in login.dto

--- src/modules/auth/dto/login.dto.ts
+++ src/modules/auth/dto/login.dto.ts
@@ -1,4 +1,10 @@
-import { IsEmail, IsNotEmpty, MaxLength, MinLength } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class LoginDto {
   @IsEmail()
@@ -9,5 +15,6 @@ export class LoginDto {
   @IsNotEmpty()
   @MinLength(8)
   @MaxLength(72)
+  @IsString()
   password!: string;
 }
