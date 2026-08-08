aa4ec817c6a84e5fadb2fde3200769665d6673c7
sam-amirpoor <amirpoorDev@gmail.com>
2026-08-08 07:24:02 +0330

    feat: inject entities to repositories

--- src/modules/monitoring/monitoring.service.ts
+++ src/modules/monitoring/monitoring.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { MonitoringResult } from './entities/monitoring-result.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class MonitoringService {}
+export class MonitoringService {
+  constructor(
+    @InjectRepository(MonitoringResult)
+    private readonly monitoringResultsRepository: Repository<MonitoringResult>,
+  ) {}
+}
--- src/modules/websites/websites.service.ts
+++ src/modules/websites/websites.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Website } from './entities/website.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class WebsitesService {}
+export class WebsitesService {
+  constructor(
+    @InjectRepository(Website)
+    private readonly websitesRepository: Repository<Website>,
+  ) {}
+}
